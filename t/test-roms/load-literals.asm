de0da0f3f0bd71c778ab5e9be9e46d0d4accfd09
Aaron France <aaron.l.france@gmail.com>
2018-08-24 19:26:00 +0200

    Drop load-test register transfer instructions
    The same tests are in another ROM

---
+++ t/test-roms/load-literals.asm
@@ -0,0 +1,9 @@
+    .ORG 0000H
+    LD A, 1
+    LD B, 2
+    LD C, 3
+    LD D, 4
+    LD E, 5
+    LD H, 6
+    LD L, 7
+    HALT
--- t/test-roms/load-tests.asm
+++ t/test-roms/load-tests.asm
@@ -6,11 +6,4 @@
     LD E, D
     LD H, E
     LD L, H
-    LD A, 1
-    LD B, 2
-    LD C, 3
-    LD D, 4
-    LD E, 5
-    LD H, 6
-    LD L, 7
     HALT
