bb003ff5c9a8fd56ef147b52456e6183f792e811
Aaron France <aaron.l.france@gmail.com>
2018-08-24 16:58:17 +0200

    Expand register load test rom

--- t/test-roms/load-tests.asm
+++ t/test-roms/load-tests.asm
@@ -3,11 +3,14 @@
     LD B, A
     LD C, B
     LD D, C
-    LD H, D
+    LD E, D
+    LD H, E
     LD L, H
     LD A, 1
     LD B, 2
     LD C, 3
     LD D, 4
-    LD H, 5
+    LD E, 5
+    LD H, 6
+    LD L, 7
     HALT
