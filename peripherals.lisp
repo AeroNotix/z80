65593de3f8fdc3af730cc00114c5cfcc73c80667
Aaron France <aaron.l.france@gmail.com>
2018-08-26 00:59:57 +0200

    Add simple peripheral that'll just print to term

--- peripherals.lisp
+++ peripherals.lisp
@@ -4,5 +4,11 @@
 (defgeneric read-from (peripheral)
   (:documentation "Read a byte from the peripheral"))
 
-(defgeneric write-to (peripheral)
+(defgeneric write-to (peripheral value)
   (:documentation "Write a byte to the peripheral"))
+
+(defclass terminal-printer-peripheral () ()
+  (:documentation "Simple peripheral that prints chars to the terminal"))
+
+(defmethod write-to ((tpp terminal-printer-peripheral) (b integer))
+  (format t "~C" (code-char b)))
