9b5900b4112b2e6badf65ff9bb4bfaf66d0407b8
Aaron France <aaron.l.france@gmail.com>
2018-08-29 22:23:01 +0200

    No magic numbers in assembly

--- t/test-roms/block-move-ldir.asm
+++ t/test-roms/block-move-ldir.asm
@@ -1,7 +1,7 @@
     .ORG 0000H
     JP START
 DATA:
-    DB  $56
+    DB  $56                     ; $56 is any old data
     DB  $56
     DB  $56
     DB  $56
@@ -24,7 +24,7 @@ DATA:
 START:
     LD HL, DATA
     LD DE, DATA2
-    LD BC, 20
+    LD BC, START - DATA
     LDIR
     HALT
 DATA2:
