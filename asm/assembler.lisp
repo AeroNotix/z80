e9752287bdb6bbbd8ee69fff537d7cde28a7f810
Aaron France <aaron.l.france@gmail.com>
2018-09-04 03:48:13 +0200

    :nail_care:

--- asm/assembler.lisp
+++ asm/assembler.lisp
@@ -1,6 +1,6 @@
 (in-package :z80-asm)
 
-(defparameter 8-bit-operand-lower
+(defparameter 8-bit-operand
   '((A . #b111)
     (B . #b000)
     (C . #b001)
@@ -10,16 +10,6 @@
     (L . #b101)
     (HL . #b110)))
 
-(defparameter 8-bit-operand-higher
-  '((A . #b111000)
-    (B . #b000000)
-    (C . #b001000)
-    (D . #b010000)
-    (E . #b011000)
-    (H . #b100000)
-    (L . #b101000)
-    (HL . #b110000)))
-
 (defparameter 8-bit-opcodes
   '((LD . #x40)))
 
@@ -27,8 +17,8 @@
   (destructuring-bind (operation operand-x operand-y) assembly-operation
     (logior
      (assoc-value 8-bit-opcodes operation)
-     (assoc-value 8-bit-operand-higher operand-x)
-     (assoc-value 8-bit-operand-lower operand-y))))
+     (ash (assoc-value 8-bit-operand operand-x) 3)
+     (assoc-value 8-bit-operand operand-y))))
 
 (defun generate-all-8-bit-loads ()
   (let ((registers '(A B C D E H L HL)))
