be2cff3d844c5d6be18c836e7e737a632433820a
Aaron France <aaron.l.france@gmail.com>
2018-09-06 22:53:22 +0200

    Add proper exports

--- package.lisp
+++ package.lisp
@@ -2,4 +2,68 @@
   (:use :common-lisp)
   (:import-from :place-utils :applyf :funcallf)
   (:import-from :alexandria :assoc-value)
-  (:export #:cpu #:emulate #:emulate-rom))
+  (:export
+
+   ;; CPU
+   #:cpu
+   #:emulate
+   #:emulate-rom
+   #:execute-instruction
+   #:execute-next-instruction
+   #:pc
+   #:ram
+   #:reset-cpu
+
+   ;; I/O
+   #:peripheral
+   #:read-from
+   #:simple-io-peripheral
+   #:write-to
+
+   ;; Register functionality
+   #:reg-a
+   #:reg-a%
+   #:reg-af
+   #:reg-af%
+   #:reg-b
+   #:reg-b%
+   #:reg-bc
+   #:reg-bc%
+   #:reg-c
+   #:reg-c%
+   #:reg-d
+   #:reg-d%
+   #:reg-de
+   #:reg-de%
+   #:reg-e
+   #:reg-e%
+   #:reg-f
+   #:reg-f%
+   #:reg-h
+   #:reg-h%
+   #:reg-hl
+   #:reg-hl%
+   #:reg-hl+
+   #:reg-hl+%
+   #:reg-hl+-register
+   #:reg-i
+   #:reg-ix
+   #:reg-ix%
+   #:reg-ixh
+   #:reg-ixl
+   #:reg-iy
+   #:reg-iy%
+   #:reg-iyh
+   #:reg-iyl
+   #:reg-l
+   #:reg-l%
+   #:reg-p
+   #:reg-pc
+   #:reg-pc-c
+   #:reg-pc-p
+   #:reg-r
+   #:reg-s
+   #:reg-s%
+   #:reg-sp
+   #:reg-sp%
+   #:mem-hl))
